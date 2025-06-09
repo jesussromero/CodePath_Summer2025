0a13ead616f7233a949ac0e2b0d9fcbd31111172
Jesus Romero <jer423@drexel.edu>
2025-06-09 15:55:11 -0400

    Last few problems

--- Unit_1/Unit2C2.py
+++ Unit_1/Unit2C2.py
@@ -102,3 +102,87 @@ def final_value_after_operations(operations):
 
 #operations = ["bouncy", "bouncy", "flouncy"]
 #final_value_after_operations(operations)
+
+# if the first letter of each word concatenated is equal to s, then it's an acronym
+def is_acronym(words, s):
+    result = ''
+    if len(words) < 1:
+        return False
+
+    for word in words:
+        result += word[0]
+    
+    if result == s:
+        return True
+    return False
+
+# input: nums -> list of numbers
+# output: number of operations to make every number in nums divisible by 3
+# strat: Use modulus to determine how many steps are needed to make a number divisible by 3
+
+# 4 % 3 = 1, if it is 1, we substract 1
+# 5 % 3 = 2, if it's 2, we add 1
+
+def make_divisible_by_3(nums):
+    count = 0
+    for num in nums:
+        if (num % 3 == 1) or (num % 3 == 2):
+            count += 1
+    return print(count)
+
+
+# Make a new list of elements that are in list1 but not in list2, and reverse
+# input : list1, list2
+# output: list1 where all elements of list1 but not in list2, 
+# list2 where all elements of list2 but not iun list 1
+
+def exclusive_elemts(lst1, lst2):
+    result_lst1 = []
+    for i in lst1:
+        if i not in lst2:
+            result_lst1.append(i)
+        
+    for j in lst2:
+        if j not in lst1:
+            result_lst1.append(j)
+
+    return print(result_lst1)
+
+
+# input: 2 different strings/words
+# output: a combined string where we alternate each letter from each string
+# strat: run a for loop and start adding the word at index i
+def merge_alternately(word1, word2):
+    result = ''
+    #base cases
+    if word1 == '':
+        return word2
+    if word2 == '':
+        return word1
+    # hola
+    # alexandria
+    for i in range(len(word1)):
+        if (len(word1) > 0) and (len(word2) > 0):
+            result += word1[0] + word2[0]
+            word1 = word1[1:]
+            word2 = word2[1:]
+    if len(word1) > 0:
+        result += word1
+    if len(word2) > 0:
+        result += word2
+    return print(result)
+
+word1 = "wol"
+word2 = "oze"
+merge_alternately(word1, word2)
+
+word1 = "hfa"
+word2 = "eflump"
+merge_alternately(word1, word2)
+
+word1 = "eyre"
+word2 = "eo"
+merge_alternately(word1, word2)
+#"woozle"
+#"heffalump"
+#"eeyore"
