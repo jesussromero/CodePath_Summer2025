834a7203aae04e35414b241c1c8a5693dfaf809e
Jesus Romero <jer423@drexel.edu>
2025-06-26 12:43:42 -0400

    First three problems solved

--- Unit_3/unit_3_c1_st_pset.py
+++ Unit_3/unit_3_c1_st_pset.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 '''
 Problem 1: Post Format Validator
 You are managing a social media platform and need to ensure that posts are properly formatted. 
@@ -26,8 +28,8 @@ False
 # U: We are given a string that contains either parentheses, brackets, or curly braces. In order for
 #    the input to be valid, each character input needs to have a corresponding closing form of itself
 #    We return a boolean
-# M: We can use a Stack to keep track of what we have in so far. So we would add each character to th3
-#    stack.
+# M: We can use a Stack to keep track of what we have in so far. If the last popped element in the stack
+#    is equal to its closing, then we return False
 def is_valid_post_format(posts):
     invalid_starting = ["}", "]", ")"]
     valid_start = {")":"(", "]":"[", "}":"{"}
@@ -35,7 +37,7 @@ def is_valid_post_format(posts):
     # Immediately return if it starts with a closing bracket or length is less than 1
     if posts[0] in invalid_starting or len(posts) <= 1:
         return False
-    # Iterate over all of the characters in the string
+    # Applying logic described above
     for ch in posts:
         if ch in valid_start.values():
             my_helper.append(ch)
@@ -45,7 +47,78 @@ def is_valid_post_format(posts):
     return True
 
         
+'''
+Problem 2: Reverse User Comments Queue
+On your platform, comments on posts are displayed in the order they are received.
+However, for a special feature, you need to reverse the order of comments before displaying them.
+Given a queue of comments represented as a list of strings, reverse the order using a stack.
 
-print(is_valid_post_format("()"))
-print(is_valid_post_format("()[]{}")) # what if: ([{}])
-print(is_valid_post_format("(]"))
+def reverse_comments_queue(comments):
+    pass
+
+Example Usage:
+
+print(reverse_comments_queue(["Great post!", "Love it!", "Thanks for sharing."]))
+
+print(reverse_comments_queue(["First!", "Interesting read.", "Well written."]))
+
+Example Output:
+
+['Thanks for sharing.', 'Love it!', 'Great post!']
+['Well written.', 'Interesting read.', 'First!']
+
+'''
+# U: We receive a list of strings, where each element is a comment. We output the reversed string
+# M: We can use a queue because it follows the FIFO rule, which is exactly what we need
+def reverse_comments_queue(comments):
+    my_queue = deque()
+    result = []
+    for comment in comments:
+        my_queue.append(comment)
+    
+    for element in range(len(my_queue)):
+        result.append(my_queue.pop())
+
+    return result # works!
+
+'''
+Problem 3: Check Symmetry in Post Titles
+As part of a new feature on your social media platform, you want to highlight post titles that are symmetrical,
+meaning they read the same forwards and backwards when ignoring spaces, punctuation, and case.
+Given a post title as a string, use a new algorithmic technique the two-pointer method to determine if the title is symmetrical.
+
+def is_symmetrical_title(title):
+    pass
+
+Example Usage:
+
+print(is_symmetrical_title("A Santa at NASA"))
+print(is_symmetrical_title("Social Media")) 
+
+Example Output:
+
+True
+False
+'''
+
+# U: The problem will give us a string. We have to check if once it doesn't have any especial chars
+#    the string is the same if read the same forwards or backwards
+# M: It explicitly asks for us to use a double pointer. My strat would be to first remove especial chars
+#    and then lower everything. After that, use the double pointer strat.
+
+def is_symmetrical_title(title):
+    title = title.lower()
+    title = title.replace(" ", "")
+    left = 0
+    right = len(title) - 1
+
+    for ch in title:
+        if title[left] != title[right]:
+            return False
+        else:
+            left += 1
+            right -= 1
+    return True # Works
+
+print(is_symmetrical_title("A Santa at NASA"))
+print(is_symmetrical_title("Social Media")) 
